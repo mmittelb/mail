0355ce664e312d7854d574f8cbda1bbc54328d08
Christoph Wurst <christoph@winzerhof-wurst.at>
2019-09-04 11:55:01 +0200

    fixup! Add CKEditor as WYSIWYG editor
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/components/Composer.vue
+++ src/components/Composer.vue
@@ -130,6 +130,7 @@ import CKEditor from '@ckeditor/ckeditor5-vue'
 import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials'
 import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold'
 import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic'
+import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph'
 import debouncePromise from 'debounce-promise'
 import Multiselect from 'nextcloud-vue/dist/Components/Multiselect'
 import {translate as t} from 'nextcloud-l10n'
@@ -226,6 +227,7 @@ export default {
 					EssentialsPlugin,
 					BoldPlugin,
 					ItalicPlugin,
+					ParagraphPlugin,
 				],
 				toolbar: {
 					items: [
